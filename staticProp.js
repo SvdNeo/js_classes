2c77e066f91ff40eacf92a6f8cd1a2260b4f0312
unknown <sanjay.d@neoito.com>
2023-12-26 18:03:37 +0530

    inheritance of static method

--- staticProp.js
+++ staticProp.js
@@ -9,3 +9,26 @@ static createTodays(){
 }
 let article1 = Article.createTodays()
 console.log(article1.title)
+
+class Birds{
+    static Planet = "Earth";
+    constructor(name,speed){
+        this.name = name;
+        this.speed = speed;
+    }
+    run(speed=0){
+        this.speed += speed
+        console.log(`${this.name} flies at a speed of ${this.speed}`)
+    }
+    static compare(birdA,birdB){
+        return birdA.speed - birdB.speed
+    }
+}
+class swan extends Birds{
+    fly(){
+console.log(`${this.name} flies`)
+}
+}
+let swans = [new swan("white swan",10),new swan("black swan",5)]
+swans.sort(swan.compare)
+swans[0].fly()
