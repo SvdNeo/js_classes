b15178040414107636ec20848f22e03e0adac551
unknown <sanjay.d@neoito.com>
2023-12-26 16:16:39 +0530

    class inheritance example of employee salary

--- class_inheritance.js
+++ class_inheritance.js
@@ -18,6 +18,31 @@ class model extends car {
         return `${this.present()}  and its model is ${this.mod}`;
     }
 }
-
 const myCar = new model("Ford", "Freestyle");
 console.log(myCar.show());
+
+class employee{
+    constructor(empName,empAge,empSalary){
+    this.empName = empName;
+    this.empAge = empAge;
+    this.empSalary = empSalary;
+}
+info(){
+    document.write(`<h3>Employee class</h3> Name:${this.empName} <br> Age:${this.empAge}<br> salary:${this.empSalary} <br>`)
+}
+}
+
+class manager extends employee{
+    info(){
+        let ta = 2000;
+        let da = 5000;
+        let totalSal = this.empSalary+ta+da;
+        document.write(`<h3>Employee class</h3> Name:${this.empName} <br> Age:${this.empAge}<br> salary:${totalSal} <br>`)
+    }
+}
+
+const emp1 = new employee("Sanjay",29,25000)
+const manager1 = new manager("Lakshmikant",30,50000)
+emp1.info()
+manager1.info()
+
